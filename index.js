2719d7b05405ccdece212205624f0f7cb08e190c
eliasvolkov <ilya.j0b@yandex.ru>
2020-01-23 00:05:14 +0300

    init server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require("./config/db");
+const posts = require("./routes/posts");
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get('/', async (req,res) => {
+    await res.send('hey')
+});
+connectDB();
+
+app.use("/api/posts", posts);
+
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 5000;
+
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`listening on port ${port}`);
+});
