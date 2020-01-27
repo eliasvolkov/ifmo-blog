3de2963d6f20bc9cc918d5e74f96a33e9ce75406
Elias <ilya.j0b@yandex.ru>
2020-01-27 09:14:33 +0300

    deploy preparation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
 const posts = require("./routes/posts");
+const path = require('path')
 require('dotenv').config();
 
 app.use(express.json());
@@ -19,6 +20,14 @@ app.use("/api/posts", posts);
 
 
 const dev = process.env.NODE_ENV !== "production";
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, err => {
